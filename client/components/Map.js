622b3953759d72ce55c205ddbcfbe0965e51840f
adamweinberg <adamcweinberg@gmail.com>
2021-06-26 10:06:04 -0700

    day 2 first commit

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps'
 
-const Map = withScriptjs((withGoogleMap(() => {
+const Map = withScriptjs(withGoogleMap(() => {
+
   return (
     <GoogleMap defaultZoom={2} defaultCenter={{lat: 0, lng: 0}} />
   )
-})))
+}))
 
 export default Map
