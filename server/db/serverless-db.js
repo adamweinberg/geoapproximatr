2a27e3ee876c1cc31469bc88dd549ce0c7d45975
adamweinberg <adamc.weinberg@gmail.com>
2025-09-02 21:34:21 -0700

    more troubleshooting

--- server/db/serverless-db.js
+++ server/db/serverless-db.js
@@ -1,23 +1,18 @@
 // server/db/serverless-db.js
 const { Sequelize } = require('sequelize');
-const pg = require('pg');
 
 const databaseUrl = process.env.NETLIFY_DATABASE_URL || process.env.DATABASE_URL;
 
 console.log('Serverless DB - DATABASE_URL available:', !!process.env.DATABASE_URL);
 console.log('Serverless DB - NETLIFY_DATABASE_URL available:', !!process.env.NETLIFY_DATABASE_URL);
-console.log('Serverlet DB - Using URL:', databaseUrl ? 'YES' : 'NO');
+console.log('Serverless DB - Using URL:', databaseUrl ? 'YES' : 'NO');
 
 if (!databaseUrl) {
   throw new Error('NETLIFY_DATABASE_URL or DATABASE_URL must be set');
 }
 
-// Configure pg for serverless environment
-pg.defaults.ssl = { rejectUnauthorized: false };
-
 const db = new Sequelize(databaseUrl, {
   dialect: 'postgres',
-  dialectModule: pg,
   logging: false,
   pool: {
     max: 1,
